7587b22c49ed85b13f6e179a6d9d80b9cdac1186
cserli <1528884535@qq.com>
2018-05-14 16:43:47 +0800

    add  shuju

--- src/LollipopGoGC/t_main.go
+++ src/LollipopGoGC/t_main.go
@@ -1,74 +1,34 @@
 package main
 
-//import (
-//	"fmt"
-//)
-
-//func init() {
-//	fmt.Println("Entry LollipopGoGS")
-//	return
-//}
-
-//func main() {
-
-//	return
-//}
-
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
-
-package main
-
 import (
-	"log"
-	"net"
+	//"log"
+	//	"net"
+	"glog-master"
+	"net/http"
 
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-	"google.golang.org/grpc/reflection"
+	//	"golang.org/x/net/context"
+	//	"google.golang.org/grpc"
+	//	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	//	"google.golang.org/grpc/reflection"
 )
 
-const (
-	port = ":50051"
-)
-
-// server is used to implement helloworld.GreeterServer.
-type server struct{}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+func TJWanJiaData(w http.ResponseWriter, req *http.Request) {
+	glog.Info("httpTask is running...")
+	if req.Method == "GET" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		req.ParseForm()
+		// 获取函数
+		Protocol, bProtocol := req.Form["Protocol"]
+		Protocol2, bProtocol2 := req.Form["Protocol2"]
+		glog.Info("httpTask is running...", Protocol, bProtocol, Protocol2, bProtocol2)
+	}
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	http.HandleFunc("/TJData", TJWanJiaData) // 获取统计
+	err := http.ListenAndServe(":7878", nil)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		glog.Info("Entry nil", err.Error())
+		return
 	}
 }
-
